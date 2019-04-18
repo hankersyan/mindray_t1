4c5b0ec8aacee34ef6ddcb45ed4ded64852974a6
hankersyan <hankersyan@msn.com>
2019-04-18 10:22:17 +0800

    init

---
+++ src/main/java/io/hankers/mdi/mindray/t1/App.java
@@ -0,0 +1,13 @@
+package io.hankers.mdi.mindray.t1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
